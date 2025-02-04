d29048557a51de28dff55a002065a8e0657ab8ae
arafat709 <mdarafat112007@gmail.com>
2025-02-04 21:29:22 +0600

    task 1

---
+++ index.js
@@ -0,0 +1,9 @@
+const digitalClk = document.getElementById("digitalClock");
+
+function startTime() {
+    digitalClock = setInterval(function () {
+        digitalClk.innerHTML = new Date().toLocaleTimeString();
+    }, 1000);
+};
+
+startTime();
