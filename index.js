e910bb7e8bad918e6c4b15bc02ba97e3b9f34e3d
arafat709 <mdarafat112007@gmail.com>
2025-02-04 22:09:01 +0600

    ...

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const digitalClk = document.getElementById("digitalClock");
 let digitalClock;
 
-
 function startTime() {
     digitalClock = setInterval(function () {
         digitalClk.innerHTML = new Date().toLocaleTimeString();
